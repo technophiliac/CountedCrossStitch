3f1d7ca97b7f9badc8adcd90f41870e07d2f226f
Leslie Allison Chapman <lezlnf@gmail.com>
2013-04-18 13:52:35 -0400

    add ability to zoom the generated  layout

--- src/com/lesliechapman/countedcrossstitch/MainActivity.java
+++ src/com/lesliechapman/countedcrossstitch/MainActivity.java
@@ -2,6 +2,9 @@ package com.lesliechapman.countedcrossstitch;
 
 import java.util.ArrayList;
 
+import zoomsupport.ZoomableRelativeLayout;
+
+import android.R.layout;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
@@ -16,17 +19,30 @@ import android.graphics.drawable.shapes.RectShape;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
+import android.view.ScaleGestureDetector.SimpleOnScaleGestureListener;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends Activity {
+	
+	private ScaleGestureDetector scaleGestureDetector;
+	private ZoomableRelativeLayout _layout;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		_layout = (ZoomableRelativeLayout)findViewById(R.id.layout);
+		
+		scaleGestureDetector = new ScaleGestureDetector(this,
+		        new OnPinchListener(_layout));
+		
+		
 		drawCanvas();
 	}
 
@@ -65,7 +81,8 @@ public class MainActivity extends Activity {
 			
 	        int w = bmp.getWidth();
 	        int h = bmp.getHeight();
-	        ArrayList<Integer> colors = new ArrayList<Integer>();
+	        int squareSize = 50;
+	            		
 	        
 	        for (int x = 0; x < w; x++) {
 	        	for (int y = 0; y < h; y++) {
@@ -77,17 +94,54 @@ public class MainActivity extends Activity {
 	        		
 	        		ImageView view1 = new ImageView(this);
 	        		view1.setImageDrawable(gd);
-	        		view1.setLayoutParams(new LayoutParams(50, 50));
-	        		RelativeLayout layout = (RelativeLayout)findViewById(R.id.layout);
-	        		view1.setX(x * 50);
-	        		view1.setY(y * 50);
-	        		layout.addView(view1);
+	        		view1.setLayoutParams(new LayoutParams(squareSize, squareSize));
+	        		view1.setX(x * squareSize);
+	        		view1.setY(y * squareSize);
+	        		_layout.addView(view1);
 					
 				}
 				
-			}
+			}        
+	        
 	}
 	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+	 // TODO Auto-generated method stub
+	 scaleGestureDetector.onTouchEvent(event);
+	 return true;
+	}
 	
 
+}
+
+class OnPinchListener extends SimpleOnScaleGestureListener {
+
+    float startingSpan; 
+    float endSpan;
+    float startFocusX;
+    float startFocusY;
+    ZoomableRelativeLayout _layout;
+    
+    public OnPinchListener(ZoomableRelativeLayout layout){
+    	_layout = layout;
+    }
+
+
+    public boolean onScaleBegin(ScaleGestureDetector detector) {
+        startingSpan = detector.getCurrentSpan();
+        startFocusX = detector.getFocusX();
+        startFocusY = detector.getFocusY();
+        return true;
+    }
+
+
+    public boolean onScale(ScaleGestureDetector detector) {
+    	_layout.scale(detector.getCurrentSpan()/startingSpan, startFocusX, startFocusY);
+        return true;
+    }
+
+    public void onScaleEnd(ScaleGestureDetector detector) {
+        //_layout.restore();
+    }
 }
